ef93b07c358fff3768dd7b399d6e677e2fc6c15d
Jordan Wild <jwild@simplenexus.com>
2019-12-04 11:03:19 -0700

    much cleaner solution

---
+++ src/main/kotlin/advent19/D4Try2.kt
@@ -0,0 +1,30 @@
+package advent19
+
+import shared.AdventSolution
+
+fun main(args: Array<String>) = object : AdventSolution(
+    mapOf(),
+    mapOf(),
+    "382345-843167"
+) {
+    override fun solveProblem1(input: String): Any? {
+        return countPossiblePasswords(input)
+    }
+
+    override fun solveProblem2(input: String): Any? {
+        return countPossiblePasswords(input, true)
+    }
+
+    private fun countPossiblePasswords(input: String, pairsOnly: Boolean = false): Int {
+        val numbers = input.split('-').map(String::toInt)
+        return (numbers.first()..numbers.last()).count { i ->
+            val ca = i.toString().toCharArray()
+            ca.sorted() == ca.toList() && ca.hasAdjacentDuplication(pairsOnly)
+        }
+    }
+
+    private fun CharArray.hasAdjacentDuplication(pairsOnly: Boolean = false): Boolean {
+        return groupBy { it }.values.any { if(pairsOnly) it.size == 2 else it.size > 1 }
+    }
+
+}.solve()
