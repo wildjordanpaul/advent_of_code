b6aa8b62f42cb16d06af3d52bdd74efc9ec0dce7
Jordan Wild <jwild@simplenexus.com>
2020-12-17 09:32:43 -0700

    cleanup

--- src/main/kotlin/advent20/Day17.kt
+++ src/main/kotlin/advent20/Day17.kt
@@ -19,55 +19,45 @@ class Day17 : AdventSolution(
 ) {
 
     override fun solveProblem1(input: String): Any {
-        var map = mutableMapOf<Point3D, Boolean>()
-
-        input.split("\n").forEachIndexed { y, line ->
-            line.forEachIndexed { x, char ->
-                map[Point3D(x, y, 0)] = char == '#'
-            }
-        }
+        var map = input.split("\n").withIndex().flatMap { (y, line) ->
+            line.mapIndexed { x, char -> Point3D(x, y, 0) to (char == '#') }
+        }.toMap()
 
         repeat(6) {
-            val nextMap = mutableMapOf<Point3D, Boolean>()
-            map.forEach { (point, active) ->
-                val activeNeighborCount = point.adjacents.count { map.getOrDefault(it, false) }
-                if (active) {
-                    nextMap[point] = activeNeighborCount in listOf(2, 3)
-                } else {
-                    nextMap[point] = activeNeighborCount == 3
-                }
-                point.adjacents.filter { !nextMap.containsKey(it) }.forEach { adjacent ->
-                    nextMap[adjacent] = adjacent.adjacents.count { map.getOrDefault(it, false) } == 3
-                }
-            }
-            map = nextMap
+            map = map.keys
+                .flatMap { it.adjacents + it }
+                .toSet()
+                .map { point ->
+                    val activeNeighborCount = point.adjacents.count { map.getOrDefault(it, false) }
+                    val active = if (map.getOrDefault(point, false)) {
+                        activeNeighborCount in listOf(2, 3)
+                    } else {
+                        activeNeighborCount == 3
+                    }
+                    point to active
+                }.toMap()
         }
         return map.values.count { it }
     }
 
     override fun solveProblem2(input: String): Any {
-        var map = mutableMapOf<Point4D, Boolean>()
-
-        input.split("\n").forEachIndexed { y, line ->
-            line.forEachIndexed { x, char ->
-                map[Point4D(x, y, 0, 0)] = char == '#'
-            }
-        }
+        var map = input.split("\n").withIndex().flatMap { (y, line) ->
+            line.mapIndexed { x, char -> Point4D(x, y, 0, 0) to (char == '#') }
+        }.toMap()
 
         repeat(6) {
-            val nextMap = mutableMapOf<Point4D, Boolean>()
-            map.forEach { (point, active) ->
-                val activeNeighborCount = point.adjacents.count { map.getOrDefault(it, false) }
-                if (active) {
-                    nextMap[point] = activeNeighborCount in listOf(2, 3)
-                } else {
-                    nextMap[point] = activeNeighborCount == 3
-                }
-                point.adjacents.filter { !nextMap.containsKey(it) }.forEach { adjacent ->
-                    nextMap[adjacent] = adjacent.adjacents.count { map.getOrDefault(it, false) } == 3
-                }
-            }
-            map = nextMap
+            map = map.keys
+                .flatMap { it.adjacents + it }
+                .toSet()
+                .map { point ->
+                    val activeNeighborCount = point.adjacents.count { map.getOrDefault(it, false) }
+                    val active = if (map.getOrDefault(point, false)) {
+                        activeNeighborCount in listOf(2, 3)
+                    } else {
+                        activeNeighborCount == 3
+                    }
+                    point to active
+                }.toMap()
         }
         return map.values.count { it }
     }
