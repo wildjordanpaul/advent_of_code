200bc058a918024271b6dcda66a30dde48b89df4
Jordan Wild <jwild@simplenexus.com>
2022-12-05 22:09:41 -0700

    D6

---
+++ src/main/kotlin/advent22/Day6.kt
@@ -0,0 +1,37 @@
+package advent22
+
+import shared.AdventSolution
+
+class Day6 : AdventSolution(
+    mapOf(
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb" to 7,
+        "bvwbjplbgvbhsrlpgdmjqwftvncz" to 5,
+        "nppdvjthqldpwncqszvftbrmjlhg" to 6,
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" to 10,
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" to 11
+    ),
+    mapOf(
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb" to 19,
+        "bvwbjplbgvbhsrlpgdmjqwftvncz" to 23,
+        "nppdvjthqldpwncqszvftbrmjlhg" to 23,
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" to 29,
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" to 26,
+    )
+) {
+    override fun solveProblem1(input: String): Any? {
+        return input.indexOfMarker(4)
+    }
+
+    override fun solveProblem2(input: String): Any? {
+        return input.indexOfMarker(14)
+    }
+
+    private fun String.indexOfMarker(n: Int): Int {
+        val q = ArrayDeque<Char>()
+        return indexOfFirst { l ->
+            q.add(l)
+            if(q.size > n) q.removeFirst()
+            q.toSet().size == n
+        } + 1
+    }
+}
--- src/main/kotlin/shared/DayRunner.kt
+++ src/main/kotlin/shared/DayRunner.kt
@@ -1,7 +1,7 @@
 package shared
 
 fun main() {
-    advent22.Day5().solve(
+    advent22.Day6().solve(
         skip1 = false,
         push1 = false,
         skip2 = false,
